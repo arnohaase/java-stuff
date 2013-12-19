2c942bd5b5d43643f4579849b96e201f66f21441
Arno Haase <arno.haase@haase-consulting.com>
2013-12-19 07:02:29 +0100

    update

--- java-stuff/src/main/java/de/arnohaase/javastuff/_60_unchecker/UncheckerMain.java
+++ java-stuff/src/main/java/de/arnohaase/javastuff/_60_unchecker/UncheckerMain.java
@@ -8,4 +8,13 @@ public class UncheckerMain {
 //		Unchecker.rethrow (new IOException ("Hallo"));
 		Unchecker2.rethrow (new IOException ("Hallo"));
 	}
+
+//    void doesNotCompile() {
+//        try {
+//            Unchecker.rethrow(new IOException("Hallo"));
+//        }
+//        catch(IOException exc) {
+//            System.out.println(exc);
+//        }
+//    }
 }
